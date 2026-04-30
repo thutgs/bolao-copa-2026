7e87bb6b32cd61353ac3d973f84375c1794072fb
Arthur Souza <arthurgo.souza@gmail.com>
2026-04-30 16:20:11 -0300

    Separando abas palpites

--- frontend/src/app/Paginas/palpite/palpite.service.ts
+++ frontend/src/app/Paginas/palpite/palpite.service.ts
@@ -9,6 +9,7 @@ export class PalpiteService {
   private http = inject(HttpClient);
   private readonly API_JOGOS = 'http://localhost:3000/jogos';
   private readonly API_PALPITES = 'http://localhost:3000/palpites';
+  private readonly API_MEUS_PALPITES = 'http://localhost:3000/palpites/meus'; // <-- Nova rota
 
   private gamesSubject = new BehaviorSubject<any[]>(this.carregarCache());
   public games$ = this.gamesSubject.asObservable();
@@ -32,7 +33,13 @@ export class PalpiteService {
   }
 
   getPalpites(): Observable<any[]> {
-    return this.http.get<any[]>(this.API_PALPITES);
+    // Agora ele chama a rota específica!
+    return this.http.get<any[]>(this.API_MEUS_PALPITES);
+  }
+
+  atualizarPalpite(id: number, palpite: any): Observable<any> {
+    // Usamos PATCH (ou PUT) apontando para o ID específico do palpite
+    return this.http.patch<any>(`${this.API_PALPITES}/${id}`, palpite);
   }
 
   salvarPalpite(palpite: any): Observable<any> {
