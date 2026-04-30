eb9bf0afeb810184644083f3ddaa693a01a99f6f
Arthur Martins <arthur28.martins.ti@gmail.com>
2026-04-29 23:19:40 -0300

    Telas de /auth e /dashboard criadas e integradas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth.component';
+import { ShellLayoutComponent } from './shared/shell-layout/shell-layout.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { PalpitesComponent } from './features/palpites/palpites.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/auth', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  {
+    path: '',
+    component: ShellLayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'palpites/:jogoId', component: PalpitesComponent },
+      { path: 'ranking', component: DashboardComponent },
+      { path: 'jogos', component: DashboardComponent }
+    ]
+  }
+];
